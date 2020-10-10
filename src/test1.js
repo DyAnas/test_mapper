8b9fa2e37539da065d40ce20a89832e386d1922d
DyAnas <48991282+DyAnas@users.noreply.github.com>
2020-10-10 15:32:41 +0200

    test show floor map onclick

--- src/test1.js
+++ src/test1.js
@@ -2,8 +2,6 @@ import React, { useState, useCallback, useEffect } from "react";
 import ImageMapper from "react-image-mapper";
 import URL from './photoshop.jpg'
 import { useHistory } from "react-router-dom";
-import withMobileDialog from "@material-ui/core";
-import Dialog from 'react-bootstrap-dialog';
 import Example from "./DialogAlert";
 export default function Test1() {
     const [query, setQuery] = useState(1);
@@ -11,66 +9,33 @@ export default function Test1() {
     const [mapAreas, setMapAreas] = useState({
         name: "Floor 2",
         areas:[
-            { id: 1, shape: "poly", coords: [42,42,46,179,116,178,120,88,117,35],preFillColor: "#02f3af" , fillColor: "#7fb775"},
-            { id: 2, shape: "poly", coords: [122,88,117,176,245,174,247,86], preFillColor: "#e7df8e" ,fillColor: "#7fb775" },
-            { id: 3, shape: "poly", coords: [46,180,41,277,85,273,116,246,116,178], preFillColor: "#02f3af" , fillColor: "#7fb775"  },
-            { id: 4, shape: "poly", coords: [42,278,34,413,110,404,116,248,85,273], preFillColor: "#e7df8e" ,fillColor: "#7fb775" },
+           { id: 1, shape: "poly", coords: [42,42,46,179,116,178,120,88,117,35],preFillColor: "#02f3af" , fillColor: "#7fb775"},
+             { id: 2, shape: "poly", coords: [122,88,117,176,245,174,247,86], preFillColor: "#e7df8e" ,fillColor: "#7fb775" },
+             { id: 3, shape: "poly", coords: [46,180,41,277,85,273,116,246,116,178], preFillColor: "#02f3af" , fillColor: "#7fb775"  },
+             { id: 4, shape: "poly", coords: [42,278,34,413,110,404,116,248,85,273], preFillColor: "#e7df8e" ,fillColor: "#7fb775" },
             { id: 5, shape: "poly", coords: [149,255,151,279,142,281,146,314,165,310,171,336,296,317,283,229], preFillColor: "#02f3af" , fillColor: "#7fb775"  } ,
             { id: 6, shape: "poly", coords: [249,85,246,173,383,172,381,162,401,162,439,79], preFillColor: "#e5787c",fillColor: "#7fb775"  },
-          //  { id: 7, shape: "poly", coords: [361,227,357,248,333,246,328,289,338,293,335,335,428,351,446,239], preFillColor: "#e5787c",fillColor: "#7fb775"  },
+            { id: 7, shape: "poly", coords: [361,227,357,248,333,246,328,289,338,293,335,335,428,351,446,239], preFillColor: "#e5787c",fillColor: "#7fb775"  },
         ]
     });
 
-/*    const handleUpdateMapArea = useCallback(
-        evt =>
-            updateMapArea(5, [evt.nativeEvent.layerX, evt.nativeEvent.layerY, 10]),
-        []
-    );*/
-
-    /**
-     * Update image area when updated
-     *
-     * @see https://github.com/coldiary/react-image-mapper/issues/32
-     */
     useEffect(() => {
         setQuery(Math.random());
     }, [mapAreas]);
 
- /*   const updateMapArea = (id, coords) => {
-        console.log(id, coords);
-        const areas = mapAreas.areas.map(item =>
-            item.id === id ? { ...item, coords } : item
-        );
-        setMapAreas({
-            name: mapAreas.name,
-            areas
-        });
-
-    };*/
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    // new function
-    const handleUpdateMapArea2 = useCallback(
+    const handleUpdateMapArea = useCallback(
         evt =>
             enterArea(evt),
 
         [ ]
     );
-    const [classes, setClasses]= useState({
-            className: 'span1',
-        }
-    );
-    const [index, setIndex]=useState("");
-    const [dialog, setDialog]=useState();
+   // todo find solution to remove multi object from mapareas without copy areas every time
+    // handle onclick sone
     const enterArea=( area )=> {
 
-
-            setIndex(area.id);
         // deep copy to areas (array)
         const copy =JSON.parse(JSON.stringify(mapAreas.areas));
-        copy[area.id-1].preFillColor="#10dec0";
+        // copy[area.id-1].preFillColor="#10dec0";
         setMapAreas({
             name: mapAreas.name,
             areas: copy
@@ -78,22 +43,68 @@ export default function Test1() {
         });
         handleShow();
 
-       // history.push("/dialog")
+        // history.push("/dialog")
         console.log("copy",copy);
+    }
+    // dialog handle
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+      // floor 1 handle
+    const handleClickFloor1= ()=> {
+    const color ="#ecdb9a"
+        setMapAreas({
+            name: "Floor 1",
+            areas:[
+                { id: 6, shape: "poly", coords: [249,85,246,173,383,172,381,162,401,162,439,79], preFillColor: color ,fillColor: "#7fb775"  },
+                { id: 7, shape: "poly", coords: [361,227,357,248,333,246,328,289,338,293,335,335,428,351,446,239], preFillColor: "#e5787c",fillColor: "#7fb775"  },
+            ]
+        });
+    }
+
+// handle onclick floor 2
+    const handleClickFloor2= ()=> {
+        setMapAreas({
+            name: "Floor 2",
+            areas: [
+                { id: 2, shape: "poly", coords: [122,88,117,176,245,174,247,86], preFillColor: "#e7df8e" ,fillColor: "#7fb775" },
+                { id: 5, shape: "poly", coords: [149,255,151,279,142,281,146,314,165,310,171,336,296,317,283,229], preFillColor: "#02f3af" , fillColor: "#7fb775"  } ,
+                { id: 6, shape: "poly", coords: [249,85,246,173,383,172,381,162,401,162,439,79], preFillColor: "#e5787c",fillColor: "#7fb775"  },
+                { id: 7, shape: "poly", coords: [361,227,357,248,333,246,328,289,338,293,335,335,428,351,446,239], preFillColor: "#e5787c",fillColor: "#7fb775"  },
+            ]
+        });
+    }
+// handle onclick floor 3
+    const handleClickFloor3= ()=> {
+        setMapAreas({
+            name: "Floor 3",
+            areas:[
+                { id: 2, shape: "poly", coords: [122,88,117,176,245,174,247,86], preFillColor: "#e7df8e" ,fillColor: "#7fb775" },
+                { id: 5, shape: "poly", coords: [149,255,151,279,142,281,146,314,165,310,171,336,296,317,283,229], preFillColor: "#02f3af" , fillColor: "#7fb775"  } ,
+                { id: 6, shape: "poly", coords: [249,85,246,173,383,172,381,162,401,162,439,79], preFillColor: "#e5787c",fillColor: "#7fb775"  },
+                { id: 7, shape: "poly", coords: [361,227,357,248,333,246,328,289,338,293,335,335,428,351,446,239], preFillColor: "#e5787c",fillColor: "#7fb775"  },
+            ]
+
+        });
+
+
     }
 
 
-
     return (
-        <div className="center">
+        <div className="container">
             <Example
                 show={show}
                 onHide={handleClose}
-                index={index}
+
                 name={mapAreas.name}
 
             />
+
+            <h2>{mapAreas.name}</h2>
         <div className="container">
+
 
             <ImageMapper
                 src={`${URL}?${query}`}
@@ -101,16 +112,16 @@ export default function Test1() {
                // onImageClick={handleUpdateMapArea}
                 map={mapAreas}
                 width={500}
-                onClick={handleUpdateMapArea2}
+                onClick={enterArea}
             />
 
             <span className="span1" >
-                   Sone a
+                   Sone d
              </span>
 
 
             <span className="span2"  >
-            Sone  b
+            Sone  j
         </span>
 
             <span className="span3">
@@ -118,24 +129,26 @@ export default function Test1() {
         </span>
 
             <span className="span4">
-            Sone d
-        </span>
-
-            <span className="span5">
             Sone e
         </span>
 
-            <span className="span6">
+            <span className="span5">
             Sone f
         </span>
-            {/*
+
+            <span className="span6">
+            Sone b
+        </span>
+
             <span className="span7">
-            Sone {mapAreas.areas[6].id}
-        </span>*/}
+            Sone a
+        </span>
         </div>
-    <cnter>
-        <h2>{index}</h2>
-    </cnter>
+            <div className="container">
+            <button onClick={handleClickFloor1}>Floor 1</button>
+            <button onClick={handleClickFloor2}>Floor 2</button>
+            <button onClick={handleClickFloor3}>Floor 3</button>
+            </div>
 
         </div>
     );
